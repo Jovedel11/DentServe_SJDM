66d17dc357efd7c60f7f032af252da98ee867063
Jovedel <jovedelhermosa11@gmail.com>
2025-08-29 20:02:20 +0800

    Another Adjustments

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ try {
 
 // for react to connect
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5174',
   credentials: true, // allow cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Added more methods
   allowedHeaders: [
